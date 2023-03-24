a4b25d2ce8433392b55cb1e1d225e1482fb00448
Nika Smilga <smilgaveronika@gmail.com>
2023-03-24 18:34:37 +0300

    streamlit instruction

--- dd_analysis.py
+++ dd_analysis.py
@@ -3,6 +3,13 @@ import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
 
 
+# To see a beautiful interactive table, run with `streamlit run <filepath>`
+# To group the table properly, open Columns (right-hand sidebar), drag first `id_and_goal`,
+# then goal type column (`open_goal` or `durec_goal_1`) to Row Groups.
+# Then one row in a table will correspond to one dialog. Dialog is represented as a sequence
+# of goals/dialog acts/etc. Eacg row is clickable, so you take a closer look at each goal and
+# the corresponding utterances.
+
 def build_table(data: pd.DataFrame) -> None:
     """Creates an interactive streamlit-AgGrid table.
 
@@ -20,8 +27,8 @@ def build_table(data: pd.DataFrame) -> None:
     AgGrid(data, gridOptions=go)
 
 
-data_durec = pd.read_csv('durec_fin.tsv', sep='\t')
-data_open = pd.read_csv('open_fin.tsv', sep='\t')
+data_durec = pd.read_csv('dialog_data/processed/durec_fin.tsv', sep='\t')
+data_open = pd.read_csv('dialog_data/processed/open_fin.tsv', sep='\t')
 del data_durec['Unnamed: 0']
 del data_open['Unnamed: 0']
 st.set_page_config(page_title="DD goals", layout="wide")
